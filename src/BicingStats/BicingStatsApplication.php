9b88d2a0ef8a7319a7acda9a0cd5256f303b2d86
Alex Carol <alex.carol.c@gmail.com>
2014-09-20 14:47:55 +0200

    Added application

---
+++ src/BicingStats/BicingStatsApplication.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BicingStats;
+
+use AlexCarol\Component\Framework\Command;
+
+final class BicingStatsApplication
+{
+    public function handleCommand(Command $command)
+    {
+
+    }
+}
+
